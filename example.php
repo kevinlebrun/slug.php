9190c5ccd64c1e1897e42b93ce1ebc88abbf2d9d
Kevin Le Brun <lebrun.k@gmail.com>
2012-12-04 21:50:19 +0100

    Add some examples

--- example.php
+++ example.php
@@ -3,3 +3,12 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+$slugifier = new \Slug\Slugifier;
+
+function slugify($string) {
+    global $slugifier;
+    return $string . ' => ' . $slugifier->slugify($string) . PHP_EOL;
+}
+
+echo slugify('Hello world!');
+echo slugify('Un titre en bon français !');
